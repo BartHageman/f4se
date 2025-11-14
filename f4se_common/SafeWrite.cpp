e0db4cdb9015b40f6f5c35463ebbfb667a3115b1
Bart Hageman <barthageman1@gmail.com>
2025-11-14 17:53:01 +0100

    DOCS: Add comments to SafeWrite in f4se_common

--- f4se_common/SafeWrite.cpp
+++ f4se_common/SafeWrite.cpp
@@ -1,66 +1,77 @@
 #include "SafeWrite.h"
 
-void SafeWriteBuf(uintptr_t addr, void * data, size_t len)
-{
-	UInt32	oldProtect;
+void SafeWriteBuf(uintptr_t addr, void *data, size_t len) {
+  UInt32 oldProtect;
 
-	VirtualProtect((void *)addr, len, PAGE_EXECUTE_READWRITE, &oldProtect);
-	memcpy((void *)addr, data, len);
-	VirtualProtect((void *)addr, len, oldProtect, &oldProtect);
+  VirtualProtect((void *)addr, len, PAGE_EXECUTE_READWRITE, &oldProtect);
+  memcpy((void *)addr, data, len);
+  VirtualProtect((void *)addr, len, oldProtect, &oldProtect);
 }
 
-void SafeWrite8(uintptr_t addr, UInt8 data)
-{
-	SafeWriteBuf(addr, &data, sizeof(data));
+void SafeWrite8(uintptr_t addr, UInt8 data) {
+  SafeWriteBuf(addr, &data, sizeof(data));
 }
 
-void SafeWrite16(uintptr_t addr, UInt16 data)
-{
-	SafeWriteBuf(addr, &data, sizeof(data));
+void SafeWrite16(uintptr_t addr, UInt16 data) {
+  SafeWriteBuf(addr, &data, sizeof(data));
 }
 
-void SafeWrite32(uintptr_t addr, UInt32 data)
-{
-	SafeWriteBuf(addr, &data, sizeof(data));
+void SafeWrite32(uintptr_t addr, UInt32 data) {
+  SafeWriteBuf(addr, &data, sizeof(data));
 }
 
-void SafeWrite64(uintptr_t addr, UInt64 data)
-{
-	SafeWriteBuf(addr, &data, sizeof(data));
+void SafeWrite64(uintptr_t addr, UInt64 data) {
+  SafeWriteBuf(addr, &data, sizeof(data));
 }
 
-static bool SafeWriteJump_Internal(uintptr_t src, uintptr_t dst, UInt8 op)
-{
+// Attempts to write a JMP or CALL (depending on op), at address src towards
+// address dst.
+//
+// Returns false if the destination is more than 2GB away. (x86_64 architectural
+// limitation)
+//
+// Returns true if successful.
+//
+// Internal function: not exposed externally.
+static bool SafeWriteJump_Internal(uintptr_t src, uintptr_t dst, UInt8 op) {
+// Don't add any padding between fields for alignment
+// We need the bytes to be *exactly* these bytes, because we're going to write
+// the struct as asm instructions
 #pragma pack(push, 1)
-	struct Code
-	{
-		UInt8	op;
-		SInt32	displ;
-	};
+  struct Code {
+    UInt8 op;     // Operation, which is either JMP (E9) or CALL (E8)
+    SInt32 displ; // Displacement or offset. (How far we jump forwards or
+                  // backwards. JMPs are relative!)
+  };
 #pragma pack(pop)
 
-	STATIC_ASSERT(sizeof(Code) == 5);
+  // If this is not exactly 5 bytes, we have done something wrong, and we
+  // should stop right here, before we muck things up even further.
+  STATIC_ASSERT(sizeof(Code) == 5);
 
-	ptrdiff_t delta = dst - (src + sizeof(Code));
-	if((delta < INT_MIN) || (delta > INT_MAX))
-		return false;
+  // Since JMPs are relative, we need to figure out the offset from the src
+  // and dest addresses. Due to a limitation in x86_64, we can't jump more
+  // than 2GB in either direction. If the distance is larger, we abort, and
+  // return false
+  ptrdiff_t delta = dst - (src + sizeof(Code));
+  if ((delta < INT_MIN) || (delta > INT_MAX))
+    return false;
 
-	Code code;
+  Code code;
 
-	code.op = op;
-	code.displ = delta;
+  code.op = op;
+  code.displ = delta;
 
-	SafeWriteBuf(src, &code, sizeof(code));
+  // If all is well, set up the struct and write the instructions to the source.
+  SafeWriteBuf(src, &code, sizeof(code));
 
-	return true;
+  return true;
 }
 
-bool SafeWriteJump(uintptr_t src, uintptr_t dst)
-{
-	return SafeWriteJump_Internal(src, dst, 0xE9);
+bool SafeWriteJump(uintptr_t src, uintptr_t dst) {
+  return SafeWriteJump_Internal(src, dst, 0xE9);
 }
 
-bool SafeWriteCall(uintptr_t src, uintptr_t dst)
-{
-	return SafeWriteJump_Internal(src, dst, 0xE8);
+bool SafeWriteCall(uintptr_t src, uintptr_t dst) {
+  return SafeWriteJump_Internal(src, dst, 0xE8);
 }
--- f4se_common/SafeWrite.h
+++ f4se_common/SafeWrite.h
@@ -1,13 +1,14 @@
 #pragma once
 
-void SafeWriteBuf(uintptr_t addr, void * data, size_t len);
-void SafeWrite8(uintptr_t addr, UInt8 data);
-void SafeWrite16(uintptr_t addr, UInt16 data);
-void SafeWrite32(uintptr_t addr, UInt32 data);
-void SafeWrite64(uintptr_t addr, UInt64 data);
+// Sets read/write/execute permission on memory page of addr, copy data to addr, then restore permissions
+void SafeWriteBuf(uintptr_t addr, void * data, size_t len); // Copy data of size len
+void SafeWrite8(uintptr_t addr, UInt8 data);                // Copy a byte
+void SafeWrite16(uintptr_t addr, UInt16 data);              // Copy 16 bits
+void SafeWrite32(uintptr_t addr, UInt32 data);              // Copy 32 bits
+void SafeWrite64(uintptr_t addr, UInt64 data);              // Copy 64 bits
 
 // ### warning: if you try to branch more than +/- 2GB with these, they will fail and return false
 // ### this is a limitation of the 'jmp' instruction and more generally the x64 ISA
 // 5 bytes written to src
-bool SafeWriteJump(uintptr_t src, uintptr_t dst);
-bool SafeWriteCall(uintptr_t src, uintptr_t dst);
+bool SafeWriteJump(uintptr_t src, uintptr_t dst); // Writes JMP at src to dest, returns whether successful
+bool SafeWriteCall(uintptr_t src, uintptr_t dst); // Writes CALL at src to dest, returns whether successful
